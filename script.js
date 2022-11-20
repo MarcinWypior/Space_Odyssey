4e55b773d8ba43d10bc7e993bfe1b29716e6bd28
@Marcin1136 <marcinwypior1990@gmail.com>
2022-11-20 19:49:07 +0100

    first commit

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ var dy=0;
 var velocity=0;
 var dangle=0;
 
+var starAngle=0;
 
 function draw(){    
     
@@ -36,10 +37,10 @@ function draw(){
     
   if (keys && keys[37]) {
       //dx= -1
-      dangle = -0.4; }
+      dangle = -0.7; }
   if (keys && keys[39]) {
       //dx=1;
-      dangle = 0.4;  
+      dangle = 0.7;  
   }
   if (keys && keys[38]) {velocity = -2; }
   if (keys && keys[40]) {velocity = 2; }
@@ -49,39 +50,42 @@ function draw(){
     
     angle=angle+dangle;
  
-    //ctx.save();
     ctx.clearRect(0,0,game.width,game.height);
-    //ctx.translate(xpos-20,ypos-30);
-    //ctx.rotate(angle);
-    //ctx.drawImage(Starship,xpos-20,ypos-30,40,60);
+    ctx.font = "30px Comic Sans MS";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "center";
+        
+    
     drawImageRot(Starship,xpos,ypos,40,60,angle);
-    //ctx.rotate(-angle);
-   // ctx.translate(-xpos+20,-ypos+30);
-    //ctx.restore();
-   
-    ctx.drawImage(Star,100,100,40,40);
+    if((Collision(100,100,40,40)<10)){
+        ctx.fillText("Kolizja", innerWidth/2, innerHeight/2);
+    }
+    
+    ctx.fillText("x :"+Math.round(xpos)+" "+ " y: "+Math.round(ypos), 100, innerHeight-50);
+    ctx.fillText("collistion "+Math.round(Collision(100,100,40,40)), 100, innerHeight-100);
+    ctx.fillText("distance "+Math.round(Distance(xpos,ypos,40,40,100,100,40,40)), 100, innerHeight-150);
+    
+   drawImageRot(Star,100,100,40,40,starAngle);
+    starAngle = starAngle +0.4;
+    if(starAngle >360)
+        starAngle=0;
+    //ctx.drawImage(Star,100,100,40,40);
     
 }
 
 function drawImageRot(img,x,y,width,height,deg){
     // Store the current context state (i.e. rotation, translation etc..)
-    //ctx.save()
-
+    //ctx.save();
     //Convert degrees to radian 
     var rad = deg * Math.PI / 180;
-
     //Set the origin to the center of the image
     ctx.translate(x + width / 2, y + height / 2);
-
     //Rotate the canvas around the origin
     ctx.rotate(rad);
-
     //draw the image    
     ctx.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
       ctx.rotate(-rad);
-    
     ctx.translate((x + width / 2)*(-1), (y + height / 2)*(-1));
-
     // Restore canvas state as saved from above
     //ctx.restore();
 }
@@ -93,6 +97,8 @@ function Obstacle(px,py,pwidth,pheight){
  this.height=pheight;
 }
 
+
+
 function MyShip(px,py,pwidth,pheight,pangle)
 {
 this.x=px;
@@ -100,6 +106,19 @@ this.x=px;
  this.width=pwidth;
  this.height=pheight;
  this.angle=pangle;
+}
+
+function Collision(x,y,width,height)
+{ 
+   
+    return Math.sqrt(x+width/2-(xpos-20)*(x+width/2-(xpos-20))+
+                      (y+height/2-(ypos+20))*(y+height/2-(ypos+20)));
+}
+
+function Distance(x1,y1,width1,height1,x2,y2,width2,height2){
+    
+    return Math.sqrt(((x1-width1/2)-(x2-width2/2))*((x1-width1/2)-(x2-width2/2))
+                    +((y1-height1/2)-(y2-height2/2))*((y1-height2/2)-(y2-height2/2)));
 }
 
 
